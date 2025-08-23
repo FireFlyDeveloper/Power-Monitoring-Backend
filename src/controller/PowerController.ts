9612cefda20daa71953d02fdc145d27b67d1fd2d
Kim <saludeskimdev@gmail.com>
2025-08-24 01:01:51 +0800

    Add MeasurementController for creating and retrieving measurements

---
+++ src/controller/PowerController.ts
@@ -0,0 +1,24 @@
+import { Context } from "hono";
+import { createRecord, selectByDate } from "../service/powerService";
+
+export class MeasurementController {
+  async create(ctx: Context) {
+    const { temperature, rpm, kwh, voltage } = await ctx.req.json();
+    const measurement = await createRecord(temperature, rpm, kwh, voltage);
+
+    return ctx.json({
+      message: "Measurement created successfully",
+      data: measurement,
+    });
+  }
+
+  async getByDate(ctx: Context) {
+    const date = ctx.req.query("date");
+    const measurements = await selectByDate(date);
+
+    return ctx.json({
+      message: `Measurements for ${date ?? "today"}`,
+      data: measurements,
+    });
+  }
+}
